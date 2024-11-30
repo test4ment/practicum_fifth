3a12b4ec35717079deb53a67b3e3666dd0c7fcb7
test4ment <nkaliczkij@yandex.ru>
2024-11-30 14:05:57 +0600

    moved pages to another file

---
+++ api_test/pages_classes.py
@@ -0,0 +1,34 @@
+sample_booking = {
+                    "firstname" : "Michael",
+                    "lastname" : "White",
+                    "totalprice" : 1337,
+                    "depositpaid" : False,
+                    "bookingdates" : {
+                        "checkin" : "9999-31-12",
+                        "checkout" : "10000-99-99"
+                    },
+                    "additionalneeds" : "Playwright autotest"
+                }
+
+class BaseTestUrl:
+    def __init__(self) -> None:
+        raise NotImplementedError()
+    
+    def make_request(self) -> str:
+        return self.url
+    
+    def make_data(self) -> dict[str, str | int | bool | dict | list]:
+        return self.data
+    
+class GetTokenUrl(BaseTestUrl):
+    def __init__(self) -> None:
+        self.url = "/auth"
+        self.data = {
+            "username": "admin",
+            "password": "password123"
+        }
+
+class CreateBookingUrl(BaseTestUrl):
+    def __init__(self) -> None:
+        self.url = "/booking"
+        self.data = sample_booking
--- api_test/test_api.py
+++ api_test/test_api.py
@@ -1,23 +1,12 @@
 from playwright.sync_api import Playwright, APIRequestContext
 import pytest
 from typing import Generator
+from pages_classes import GetTokenUrl, CreateBookingUrl, sample_booking
 
 headers = {
         "Accept": "application/json",
         "Content-Type": "application/json"
         }
-
-sample_booking = {
-                    "firstname" : "Michael",
-                    "lastname" : "White",
-                    "totalprice" : 1337,
-                    "depositpaid" : False,
-                    "bookingdates" : {
-                        "checkin" : "9999-31-12",
-                        "checkout" : "10000-99-99"
-                    },
-                    "additionalneeds" : "Playwright autotest"
-                }
 
 
 @pytest.fixture(scope="session")
@@ -60,27 +49,3 @@ def test_booking_create(api_request_context: APIRequestContext):
 
     assert all([key in resp.json() for key in {"booking", "bookingid"}])
     assert all([key in resp.json()["booking"] for key in sample_booking.keys()])
-    
-
-class BaseTestUrl:
-    def __init__(self) -> None:
-        raise NotImplementedError()
-    
-    def make_request(self) -> str:
-        return self.url
-    
-    def make_data(self) -> dict[str, str | int | bool | dict | list]:
-        return self.data
-    
-class GetTokenUrl(BaseTestUrl):
-    def __init__(self) -> None:
-        self.url = "/auth"
-        self.data = {
-            "username": "admin",
-            "password": "password123"
-        }
-
-class CreateBookingUrl(BaseTestUrl):
-    def __init__(self) -> None:
-        self.url = "/booking"
-        self.data = sample_booking
